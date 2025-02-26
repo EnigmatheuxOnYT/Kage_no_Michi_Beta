cab4c7126259c16e5894ae1be0fa1ee31a4e4134
EnigmatheuxOnYT <legorex75@gmail.com>
2025-02-26 19:34:45 +0100

    Supression du dossier data

--- 2025_ID_Kage_no_Michi/data/map/map/maison 1 village d_INE.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.11.0" name="maison 1 village d_INE" tilewidth="32" tileheight="32" tilecount="54" columns="9">
- <image source="objet/maison 1 village d_INE.png" width="300" height="200"/>
-</tileset>
