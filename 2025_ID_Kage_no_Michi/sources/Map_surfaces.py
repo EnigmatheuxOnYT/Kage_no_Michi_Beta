a66a92809f028625627ef0682ca164ec044956fc
Clem4096 <clem4097@gmail.com>
2025-02-28 00:39:32 +0100

    Trucs

--- 2025_ID_Kage_no_Michi/sources/Map_surfaces.py
+++ 2025_ID_Kage_no_Michi/sources/Map_surfaces.py
@@ -12,12 +12,12 @@ Created on Thu Feb 27 23:18:15 2025
 import pygame
 
 surfaces_32x32 = {"none":pygame.surface.Surface((32,32)),
-                  "placeholder":pygame.image.load("../data/assets/map/32x32_placehlder.png"),
-                  "money_bag":pygame.image.load("../data/assets/map/32x32_placehlder.png"),
-                  "heal_potion":pygame.image.load("../data/assets/map/32x32_placehlder.png"),
-                  "food1":pygame.image.load("../data/assets/map/32x32_placehlder.png"),
-                  "food2":pygame.image.load("../data/assets/map/32x32_placehlder.png"),
-                  "food3":pygame.image.load("../data/assets/map/32x32_placehlder.png"),
-                  "food4":pygame.image.load("../data/assets/map/32x32_placehlder.png"),
-                  "food5":pygame.image.load("../data/assets/map/32x32_placehlder.png"),
+                  "placeholder":pygame.image.load("../data/assets/map/32x32_placeholder.png"),
+                  "money_bag":pygame.image.load("../data/assets/map/32x32_placeholder.png"),
+                  "heal_potion":pygame.image.load("../data/assets/map/32x32_placeholder.png"),
+                  "food1":pygame.image.load("../data/assets/map/32x32_placeholder.png"),
+                  "food2":pygame.image.load("../data/assets/map/32x32_placeholder.png"),
+                  "food3":pygame.image.load("../data/assets/map/32x32_placeholder.png"),
+                  "food4":pygame.image.load("../data/assets/map/32x32_placeholder.png"),
+                  "food5":pygame.image.load("../data/assets/map/32x32_placeholder.png"),
                   }
