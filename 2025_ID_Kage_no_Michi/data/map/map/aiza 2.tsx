6d981d616820a69186c9c7e8e0d541b0ff6f56bc
Clem4096 <clem4097@gmail.com>
2025-03-01 17:13:37 +0100

    Add files via upload
    map

---
+++ 2025_ID_Kage_no_Michi/data/map/map/aiza 2.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.0" name="aiza 2" tilewidth="32" tileheight="32" tilecount="182" columns="14">
+ <image source="objet/aiza 2.png" width="460" height="440"/>
+</tileset>
