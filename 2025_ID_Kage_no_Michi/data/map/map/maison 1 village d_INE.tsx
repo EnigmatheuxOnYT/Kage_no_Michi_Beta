f093caef5329b6ae98abfcd488a48388f8a5532e
EnigmatheuxOnYT <legorex75@gmail.com>
2025-03-03 22:58:39 +0100

    update

--- 2025_ID_Kage_no_Michi/data/map/map/maison 1 village d_INE.tsx
+++ 2025_ID_Kage_no_Michi/data/map/map/maison 1 village d_INE.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.11.0" name="maison 1 village d_INE" tilewidth="32" tileheight="32" tilecount="54" columns="9">
- <image source="../../../../../../2025_ID_Kage_no_Michi/data/map/map/objet/maison 1 village d_INE.png" width="300" height="200"/>
+<tileset version="1.10" tiledversion="1.11.0" name="maison 1 village d_INE" tilewidth="32" tileheight="32" tilecount="342" columns="18">
+ <image source="dojo 1 vILLAGE D_INE.png" width="600" height="628"/>
 </tileset>
--- 2025_ID_Kage_no_Michi/data/map/map/maison 2 village d_INE.tsx
+++ 2025_ID_Kage_no_Michi/data/map/map/maison 2 village d_INE.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.10" tiledversion="1.11.0" name="maison 2 village d_INE" tilewidth="32" tileheight="32" tilecount="20" columns="4">
- <image source="../../../../../../2025_ID_Kage_no_Michi/data/map/map/objet/maison 2 village d_INE.png" width="129" height="169"/>
+ <image source="maison 2 village d'INE.png" width="129" height="169"/>
 </tileset>
