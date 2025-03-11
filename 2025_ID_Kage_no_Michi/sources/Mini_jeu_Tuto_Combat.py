916c16c073b3f98ef881348bc6dc66d6a62377f2
Alp1an <alptankorkmaz@gmail.com>
2025-03-11 21:30:42 +0100

    Fix bug animations de Musashi (Il faut avoir les animations de Musashi Jeune si on veut l' implémenter)

--- 2025_ID_Kage_no_Michi/sources/Mini_jeu_Tuto_Combat.py
+++ 2025_ID_Kage_no_Michi/sources/Mini_jeu_Tuto_Combat.py
@@ -62,11 +62,10 @@ class minigm_tutofight :
     ########## Démarrage du mini-jeu ##########
     def load (self):
         self.playing = True
-        self.fight.load ('mgm1',self.fight_assets.Musashi_jeune,[],[self.fight_assets.pantin_de_combat],1)
+        self.fight.load ('mgm1',self.fight_assets.Musashi,[],[self.fight_assets.pantin_de_combat],1)
      
     def load_assets(self):
         # Importer les images, sons etc.. ici (depuis "../data/assets")
-        
         
         ### Importation de la police d'écriture (taille des textes des dialogues)
         self.font_MFMG30 = pygame.font.Font("../data/assets/fonts/MadouFutoMaruGothic.ttf",30)
@@ -122,10 +121,6 @@ class minigm_tutofight :
             pygame.event.post(event)
         
         self.fight.handle_imput()
-        
-
-        
-    
     
     ########## Partie 2 : Mise à jour ##########
     def minigm_update (self):
